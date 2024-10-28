08690b45c7661aedb6a4117926f8988714eab2da
Zhimeng Xin <162425451+zhimengXin@users.noreply.github.com>
2024-10-28 17:11:55 +0800

    Update run_coco_base.sh

--- run_coco_base.sh
+++ run_coco_base.sh
@@ -1,11 +1,10 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --config-file configs/coco/scsm_det_r101_base.yaml \
 	OUTPUT_DIR "output/coco/base" \
         MODEL.BACKBONE.WITHSCSM True \
-        # MODEL.BACKBONE.WITHECEA = False
-        # MODEL.BACKBONE.FREEZE_ECEA = False
+        # MODEL.BACKBONE.WITHSFA = False
+        # MODEL.BACKBONE.FREEZE_SFA = False
         # MODEL.BACKBONE.WITHMAMBA = False
         # MODEL.BACKBONE.WITHSCSM = False
         # MODEL.BACKBONE.WITHSFASA = False
         # MODEL.BACKBONE.WITHSFASENet = False
         # MODEL.BACKBONE.CNN = False
-        # MODEL.BACKBONE.ATTACK = False
