8a07cf12e07c327cb55696d95ecd85217bbc4055
Zhimeng Xin <162425451+zhimengXin@users.noreply.github.com>
2025-09-23 17:49:29 +0800

    Update run_coco_base.sh to comment model options
    Comment out additional model backbone options.

--- run_coco_base.sh
+++ run_coco_base.sh
@@ -1,10 +1,10 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --config-file configs/coco/scsm_det_r101_base.yaml \
 	OUTPUT_DIR "output/coco/base" \
         MODEL.BACKBONE.WITHSCSM True \
-        # MODEL.BACKBONE.WITHSFA = False
-        # MODEL.BACKBONE.FREEZE_SFA = False
+        # MODEL.BACKBONE.WITHECEA = False
+        # MODEL.BACKBONE.FREEZE_ECEA = False
         # MODEL.BACKBONE.WITHMAMBA = False
         # MODEL.BACKBONE.WITHSCSM = False
-        # MODEL.BACKBONE.WITHSFASA = False
-        # MODEL.BACKBONE.WITHSFASENet = False
+        # MODEL.BACKBONE.WITHECEASA = False
+        # MODEL.BACKBONE.WITHECEASENet = False
         # MODEL.BACKBONE.CNN = False
